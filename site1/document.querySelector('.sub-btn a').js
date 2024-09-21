52302e12e37ca43eb5b06f73711c7f026eb3a01b
Samuel George <171848850+sammyoflightup@users.noreply.github.com>
2024-09-21 05:12:09 -0700

    Delete site1 directory

--- site1/document.querySelector('.sub-btn a').js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.sub-btn a').addEventListener('click', function (e) {
-    e.preventDefault(); // Prevent the default action
-    const button = this;
-    button.classList.add('active');
-    setTimeout(() => {
-        button.classList.remove('active');
-    }, 5000);
-});
