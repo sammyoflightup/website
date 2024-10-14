87fad493cb9b1fc23da6ab7e6b7b84f2f5d58cfa
Samuel George <171848850+sammyoflightup@users.noreply.github.com>
2024-10-14 08:09:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -29,7 +29,36 @@ function adjustLayout() {
             software.style.width = '120px'; // Default width
         });
     }
-
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+            },
+        },
+    });
+    
     // Additional adjustments can be added here
 }
 
